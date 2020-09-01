57186b4e306124d1051a9d9b86e6dca0b3478ec9
LiaoFJ <50604988+LiaoFJ@users.noreply.github.com>
2020-09-01 02:02:32 +0100

    Update Final_model.py

--- Final_model.py
+++ Final_model.py
@@ -29,10 +29,10 @@ class Final_model(nn.Module):
         # self.Draw_model_4 = D.DRAWModel(8, 5, 5, 128, 2, 2, params)
 
 
-        self.Draw_model_1 = V.VAE(16, 64, 4096, 300, self.device)
-        self.Draw_model_2 = V.VAE(32, 32, 4096, 200, self.device)
-        self.Draw_model_3 = V.VAE(64, 16, 2048, 200, self.device)
-        self.Draw_model_4 = V.VAE(128, 8, 1024, 100, self.device)
+        self.Draw_model_1 = V.VAE(16, 64, 3500, 300, self.device)
+        self.Draw_model_2 = V.VAE(32, 32, 3500, 200, self.device)
+        self.Draw_model_3 = V.VAE(64, 16, 2000, 200, self.device)
+        self.Draw_model_4 = V.VAE(128, 8, 1000, 100, self.device)
 
 
 
