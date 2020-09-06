ce4a09b2f389cc07b3f628b41399bf36f4f17b1a
LiaoFJ <50604988+LiaoFJ@users.noreply.github.com>
2020-09-06 02:29:56 +0100

    Update Final_model.py

--- Final_model.py
+++ Final_model.py
@@ -63,7 +63,7 @@ class Final_model(nn.Module):
         return torch.sigmoid(x_model_output)
 
     def loss(self, x):
-        # x_recon = self.forward(x)
+        x_recon = self.forward(x)
         latent_loss = self.loss_1 + self.loss_2 + self. loss_3 + self.loss_4
         # print('latent_loss: ', latent_loss)
         # criterion = nn.MSELoss()
