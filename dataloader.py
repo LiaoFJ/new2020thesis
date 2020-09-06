4d7a892d2c3e16167ea3786a5a651a794853394c
LiaoFJ <50604988+LiaoFJ@users.noreply.github.com>
2020-09-06 21:27:56 +0100

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -17,7 +17,6 @@ def get_data(params):
     transform = transforms.Compose([
         transforms.Resize((64, 64)),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
     # Create the dataset.
